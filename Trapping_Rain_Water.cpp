9bc14584a390f4376c185741ad412c8e7e86b954
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-02-10 20:18:20 +0300

    Create Trapping_Rain_Water.cpp

---
+++ Trapping_Rain_Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int l = 0;
+        int r =  height.size() - 1;
+        int max_l = height[l];
+        int max_r = height[r];
+        while (l < r) {
+            if (max_l <= max_r) {
+                l += 1;
+                max_l = max_l > height[l] ? max_l : height[l];
+                ans += max_l - height[l];
+            } else {
+                r -= 1;
+                max_r = max_r > height[r] ? max_r : height[r];
+                ans += max_r - height[r];
+            }
+        }
+        return ans;
+    }
+};
