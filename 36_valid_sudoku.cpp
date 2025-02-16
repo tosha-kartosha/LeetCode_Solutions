81c97d13d5aefbfc68b5daf4df101fe18744eceb
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-02-16 11:53:09 +0300

    Create 36_valid_sudoku.cpp

---
+++ 36_valid_sudoku.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        std::unordered_map<int, std::vector<char>> rows;
+        std::unordered_map<int, std::vector<char>> cols;
+        // pair не имеет встроенной функции хэширования
+        std::unordered_map<int, std::vector<char>> squares;
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    if (std::find(rows[r].begin(), rows[r].end(), board[r][c]) != rows[r].end() ||
+                        std::find(cols[c].begin(), cols[c].end(), board[r][c]) != cols[c].end() ||
+                        std::find(squares[(r/3)*3+c/3].begin(), squares[(r/3)*3+c/3].end(), board[r][c]) != squares[(r/3)*3+c/3].end()) {
+                            return false;
+                        } else {
+                            rows[r].push_back(board[r][c]);
+                            cols[c].push_back(board[r][c]);
+                            squares[(r/3)*3+c/3].push_back(board[r][c]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
