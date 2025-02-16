ab6b7364094f5402bdbc4b1a634513e1a020ec30
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-02-16 18:18:30 +0300

    Update 36_valid_sudoku.cpp - add best solution

--- 36_valid_sudoku.cpp
+++ 36_valid_sudoku.cpp
@@ -25,6 +25,7 @@ public:
     }
 };
 
+//with unordered_set
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -44,6 +45,57 @@ public:
                             rows[r].insert(board[r][c]);
                             cols[c].insert(board[r][c]);
                             squares[(r/3)*3+c/3].insert(board[r][c]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+
+// best time and memory
+class Solution {
+public:
+    bool isNumberInRow(std::vector<std::vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; ++i) {
+            if (i != row && board[i][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isNumberInCol(std::vector<std::vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; ++i) {
+            if (i != col && board[row][i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isNumberInSqr(std::vector<std::vector<char>>& board, int row, int col, char num) {
+        int block_row = row - row % 3;
+        int block_col = col - col % 3;
+        for (int i = block_row; i < block_row + 3; ++i) {
+            for (int j = block_col; j < block_col + 3; ++j) {
+                if (i != row && j != col && board[i][j] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool isInvalid(std::vector<std::vector<char>>& board, int row, int col, char num) {
+        return (isNumberInRow(board, row, col, num) || isNumberInCol(board, row, col, num) || isNumberInSqr(board, row, col, num));
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    if (isInvalid(board, r, c, board[r][c])) {
+                        return false;
                     }
                 }
             }
