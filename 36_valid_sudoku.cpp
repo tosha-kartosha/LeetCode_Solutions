b69fa52068a289bd0967a532cbd0ec348bf0a2f3
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-02-16 12:40:45 +0300

    Update 36_valid_sudoku.cpp - add new solution with dif unordered_set

--- 36_valid_sudoku.cpp
+++ 36_valid_sudoku.cpp
@@ -24,3 +24,30 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        std::unordered_map<int, std::unordered_set<char>> rows;
+        std::unordered_map<int, std::unordered_set<char>> cols;
+        // pair не имеет встроенной функции хэширования
+        std::unordered_map<int, std::unordered_set<char>> squares;
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    if (rows[r].count(board[r][c]) > 0 ||
+                        cols[c].count(board[r][c]) > 0 ||
+                        squares[(r/3)*3+c/3].count(board[r][c]) > 0) {
+                            return false;
+                        } else {
+                            rows[r].insert(board[r][c]);
+                            cols[c].insert(board[r][c]);
+                            squares[(r/3)*3+c/3].insert(board[r][c]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
