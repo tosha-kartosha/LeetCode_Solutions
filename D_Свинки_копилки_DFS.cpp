32c7acd24e84b054ab4a3a15aa52e7f92fe9ed21
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-04-13 14:21:09 +0300

    Update D_Свинки_копилки_DFS.cpp --add explanation

--- D_Свинки_копилки_DFS.cpp
+++ D_Свинки_копилки_DFS.cpp
@@ -1,9 +1,9 @@
+// логика в том, что надо подсчитать кол-во циклов, при этом, если копилка открывается только собой, то это тоже цикл. Для каждой компоненты связности (относительно ориентированного графа!): +1, если есть цикл, 0 в противном случае: 
 #include <iostream>
 #include <vector>
 void dfs_cycles(std::vector<std::vector<int>> &graph, int v, std::vector<int> &visited, bool &IsCyclic) {
     visited[v] = 1;
     for (int to: graph[v]) {
-        // не посещена
         if (visited[to] == 0) {
             dfs_cycles(graph, to, visited, IsCyclic);
         } else if (visited[to] == 1) {
@@ -18,7 +18,7 @@ int main () {
     std::cin >> vertCount;
     int ans = 0;
     std::vector<int> visited(vertCount, 0);
-    std::vector<std::vector<int>> graph(vertCount); // список смежности
+    std::vector<std::vector<int>> graph(vertCount);
     for (int i = 0; i < vertCount; ++i) {
         int a;
         std::cin >> a;
