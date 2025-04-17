5423b03892187a4e166a381577a59dded09cc5bf
tosha-kartosha <145836322+tosha-kartosha@users.noreply.github.com>
2025-04-17 21:12:20 +0300

    Create toy_maze.cpp

---
+++ toy_maze.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+const int INF = 1e9;
+
+int bfs_maze(std::vector<std::vector<int>> &graph, int n, int m) {
+    std::vector<std::vector<int>> dist(n, std::vector<int>(m, INF));
+    std::queue<std::pair<int, int>> q;
+    int ans = INF;
+    int new_x, new_y;
+    q.push(std::pair(0, 0));
+    dist[0][0] = 0;
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        new_x = x;
+        new_y = y;
+        if (y != 0) {
+            while (new_y - 1 != -1 && graph[new_x][new_y - 1] != 1 && graph[new_x][new_y - 1] != 2) {
+                new_y--;
+            }
+            if (new_y - 1 != -1 && graph[new_x][new_y - 1] == 2) {
+                new_y--;
+            }
+            if (dist[new_x][new_y] > dist[x][y] + 1) {
+                dist[new_x][new_y] = dist[x][y] + 1;
+                if (graph[new_x][new_y] == 2) {
+                    ans = (ans > dist[new_x][new_y] ? dist[new_x][new_y] : ans);
+                } else {
+                    q.push(std::pair(new_x, new_y));
+                }
+            }
+        }
+        new_x = x;
+        new_y = y;
+        if (x != 0) {
+            while (new_x - 1 != -1 && graph[new_x - 1][new_y] != 1 && graph[new_x - 1][new_y] != 2) {
+                new_x--;
+            }
+            if (new_x - 1 != -1 && graph[new_x - 1][new_y] == 2) {
+                new_x--;
+            }
+            if (dist[new_x][new_y] > dist[x][y] + 1) {
+                dist[new_x][new_y] = dist[x][y] + 1;
+                if (graph[new_x][new_y] == 2) {
+                    ans = (ans > dist[new_x][new_y] ? dist[new_x][new_y] : ans);
+                } else {
+                    q.push(std::pair(new_x, new_y));
+                }
+            }
+        }
+        new_x = x;
+        new_y = y;
+        if (y != m-1) {
+            while (new_y + 1 != m && graph[new_x][new_y + 1] != 1 && graph[new_x][new_y + 1] != 2) {
+                new_y++;
+            }
+            if (new_y + 1 != m && graph[new_x][new_y + 1] == 2) {
+                new_y++;
+            }
+            if (dist[new_x][new_y] > dist[x][y] + 1) {
+                dist[new_x][new_y] = dist[x][y] + 1;
+                if (graph[new_x][new_y] == 2) {
+                    ans = (ans > dist[new_x][new_y] ? dist[new_x][new_y] : ans);
+                } else {
+                    q.push(std::pair(new_x, new_y));
+                }
+            }
+        }
+        new_x = x;
+        new_y = y;
+        if (x != n-1) {
+            while (new_x + 1 != n && graph[new_x + 1][new_y] != 1 && graph[new_x + 1][new_y] != 2) {
+                new_x++;
+            }
+            if (new_x + 1 != n && graph[new_x + 1][new_y] == 2) {
+                new_x++;
+            }
+            if (dist[new_x][new_y] > dist[x][y] + 1) {
+                dist[new_x][new_y] = dist[x][y] + 1;
+                if (graph[new_x][new_y] == 2) {
+                    ans = (ans > dist[new_x][new_y] ? dist[new_x][new_y] : ans);
+                } else {
+                    q.push(std::pair(new_x, new_y));
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<std::vector<int>> graph(n, std::vector<int>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cin >> graph[i][j];
+        }
+    }
+    std::cout << bfs_maze(graph, n, m);
+}
